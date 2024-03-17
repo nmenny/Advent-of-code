e9186dfe959bc21e508dad65a3a4fcf657f1eb5e
Nathan <nathanmenny@gmail.com>
2024-03-17 13:09:39 +0100

    reformat python codes

--- 2015/d3/code.py
+++ 2015/d3/code.py
@@ -1,20 +1,21 @@
 def move(x, y, direction):
-    if(direction == ">"):
+    if direction == ">":
         x += 1
-    elif (direction == "<"):
+    elif direction == "<":
         x -= 1
-    elif (direction == "^"):
+    elif direction == "^":
         y += 1
-    elif (direction == "v"):
+    elif direction == "v":
         y -= 1
 
     return x, y
 
+
 with open("./input.txt", "r") as f:
 
     delivery_map = {}
-    x_s, y_s = (0, 0) # Coordinates of Santa
-    delivery_map[(0, 0)] = 1 # Santa delivers a present to the first house
+    x_s, y_s = (0, 0)  # Coordinates of Santa
+    delivery_map[(0, 0)] = 1  # Santa delivers a present to the first house
     santa_move = True
 
     while True:
@@ -26,7 +27,7 @@ with open("./input.txt", "r") as f:
         x_s, y_s = move(x_s, y_s, direction)
         x, y = (x_s, y_s)
 
-        if((x, y) not in delivery_map):
+        if (x, y) not in delivery_map:
             delivery_map[(x, y)] = 1
         else:
             delivery_map[(x, y)] += 1
@@ -36,9 +37,11 @@ with open("./input.txt", "r") as f:
 with open("./input.txt", "r") as f:
 
     delivery_map = {}
-    x_s, y_s = (0, 0) # Coordinates of Santa
-    x_r, y_r = (0, 0) # Coordinates of robot-Santa
-    delivery_map[(0, 0)] = 2 # Santa and Robot-Santa both deliver a present to the first house
+    x_s, y_s = (0, 0)  # Coordinates of Santa
+    x_r, y_r = (0, 0)  # Coordinates of robot-Santa
+    delivery_map[(0, 0)] = (
+        2  # Santa and Robot-Santa both deliver a present to the first house
+    )
     santa_move = True
 
     while True:
@@ -54,7 +57,7 @@ with open("./input.txt", "r") as f:
             x_r, y_r = move(x_r, y_r, direction)
             x, y = (x_r, y_r)
 
-        if((x, y) not in delivery_map):
+        if (x, y) not in delivery_map:
             delivery_map[(x, y)] = 1
         else:
             delivery_map[(x, y)] += 1
--- 2015/d4/code.py
+++ 2015/d4/code.py
@@ -1,5 +1,5 @@
-
 import hashlib
+
 
 def getLowestNumberProducingNZeroesHash(n):
     trailingInt = 0
@@ -7,7 +7,7 @@ def getLowestNumberProducingNZeroesHash(n):
     result = hashlib.md5((key + str(trailingInt)).encode())
     hexHash = result.hexdigest()
 
-    while not hexHash.startswith("0"*n):
+    while not hexHash.startswith("0" * n):
         trailingInt += 1
         result = hashlib.md5((key + str(trailingInt)).encode())
         hexHash = result.hexdigest()
@@ -18,8 +18,8 @@ def getLowestNumberProducingNZeroesHash(n):
 with open("./input.txt", "r") as f:
     # initializing string
     key = f.readline()
-    if key[-1] == '\n':
-        key = key[:len(key)-1]
+    if key[-1] == "\n":
+        key = key[: len(key) - 1]
 
     print("Solution 1 :", getLowestNumberProducingNZeroesHash(5))
     print("Solution 2 :", getLowestNumberProducingNZeroesHash(6))
