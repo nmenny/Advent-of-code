488a050701ffcff89ff5a397d809db8f1fa1607d
Nathan <nathanmenny@gmail.com>
2024-03-17 13:03:32 +0100

    Add 2015 Day 19 part 2 solution (JS)
    Use a Monte-Carlo algorithm from the given molecule to find a combination leading to a single electron
    
    Close #3

--- 2015/d19/code.js
+++ 2015/d19/code.js
@@ -5,99 +5,94 @@ const events = require("events");
 class CalibrationMachine {
     constructor() {
         this.rules = {};
+        this.reverse = {};
+    }
+
+    get reverseReplacements() {
+        return this.reverse;
     }
 
     addReplacementRule(head, tail) {
-        if(!(head in this.rules)) {
+        if (!(head in this.rules)) {
             this.rules[head] = [];
         }
 
         this.rules[head].push(tail);
+        this.reverse[tail] = head;
     }
 
     *replacementsForHeadAt(molecule, headIdx) {
-        const nbCarInHead = Math.max(...Object.keys(this.rules).map(v => v.length));
+        const nbCarInHead = Math.max(...Object.keys(this.rules).map((v) => v.length));
         let head = molecule[headIdx];
 
         let exploreIdx = 0;
 
         do {
-            if(head in this.rules) {
-                for(const tail of this.rules[head]) {
+            if (head in this.rules) {
+                for (const tail of this.rules[head]) {
                     yield [tail, exploreIdx];
                 }
             }
             exploreIdx++;
-            head = molecule.slice(headIdx, headIdx+exploreIdx+1);
-        } while(exploreIdx < nbCarInHead);
-    }
-
-    *computeAllReplacement(molecule, history = new Set()) {
-        for(let idx = 0; idx < molecule.length; idx++) {
-            for(const newHead of this.replacementsForHeadAt(molecule, idx)) {
-                const newMolecule = molecule.slice(0, idx) + newHead[0] + molecule.slice(idx+1+newHead[1]);
-                if(!history.has(newMolecule)) {
-                    history.add(newMolecule);
-                    yield newMolecule;
-                }
-            }
-        }
+            head = molecule.slice(headIdx, headIdx + exploreIdx + 1);
+        } while (exploreIdx < nbCarInHead);
     }
 }
 
 function extractRule(line) {
     const splitted = line.split("=>");
 
-    if(splitted.length !== 2) throw new Error(`${line} is not a correct rule`);
+    if (splitted.length !== 2) throw new Error(`${line} is not a correct rule`);
 
     return { head: splitted[0].trim(), tail: splitted[1].trim() };
 }
 
-function fabricate(molecule, calibrationMachine, maxDepth=1000) {
-    const toExplore = ["e"];
-    const trace = {"e" : undefined};
-    const history = new Set(toExplore);
-    let depth = 0;
-
-    while(!history.has(molecule) && depth < maxDepth) {
-        depth++;
-        const nbNodesToExplore = toExplore.length;
-
-        for(let exploredCnt = 0; exploredCnt < nbNodesToExplore; exploredCnt++) {
-            const nodeToExplore = toExplore.shift();
-            history.add(nodeToExplore);
-
-            for(let newMolecule of calibrationMachine.computeAllReplacement(nodeToExplore, history)) {
-                toExplore.push(newMolecule);
-                trace[newMolecule] = nodeToExplore;
+function getNbAllReplacements(molecule, calibrationMachine) {
+    const history = new Set();
+    let nbRepl = 0;
+    for (let idx = 0; idx < molecule.length; idx++) {
+        for (const newHead of calibrationMachine.replacementsForHeadAt(molecule, idx)) {
+            const newMolecule = molecule.slice(0, idx) + newHead[0] + molecule.slice(idx + 1 + newHead[1]);
+            if (!history.has(newMolecule)) {
+                history.add(newMolecule);
+                nbRepl++;
             }
         }
     }
 
-    // showTrace(trace, molecule);
-
-    return depth;
+    return nbRepl;
 }
 
-function showTrace(trace, from) {
+function randomReverseResearch(molecule, calibrationMachine, maxStep = 1000) {
+    const replacements = Object.keys(calibrationMachine.reverseReplacements);
 
-    console.log("\n----- Process to make molecule -----");
+    let nbPerm = 0;
+    let step = 0;
+    let currMol = molecule;
 
-    let prevNode = from in trace ? trace[from] : undefined;
-    const show = [];
-    
-    show.push(prevNode ? `${prevNode} => ${from}` : `${from}`);
-    while(prevNode) {
-        const temp = prevNode in trace ? trace[prevNode] : undefined;
-        show.push(temp ? `${temp} => ${prevNode}` : `${prevNode}`);
-        prevNode = temp;
+    while (step < maxStep && currMol !== "e") {
+        const alea = Math.floor(Math.random() * replacements.length);
+        const rempl = replacements[alea];
+
+        currMol = currMol.replace(rempl, (match) => {
+            nbPerm++;
+            return calibrationMachine.reverseReplacements[match];
+        });
+
+        step++;
     }
 
-    for(let showIdx = show.length - 1; showIdx >= 0; showIdx--) {
-        console.log(show[showIdx]);
+    return currMol === "e" ? nbPerm : undefined;
+}
+
+function monteCarloSearch(molecule, calibrationMachine, nbIt = 500) {
+    let minNbPerm = Infinity;
+    for (let it = 0; it < nbIt; it++) {
+        const searchRes = randomReverseResearch(molecule, calibrationMachine);
+        if (searchRes !== undefined) minNbPerm = Math.min(minNbPerm, searchRes);
     }
 
-    console.log("\n");
+    return minNbPerm;
 }
 
 (async () => {
@@ -106,26 +101,19 @@ function showTrace(trace, from) {
     const cal = new CalibrationMachine();
     let calibrationMolecule = "";
 
-    rl.on("line", line => {
-        if(line.length === 0) return;
-        
+    rl.on("line", (line) => {
+        if (line.length === 0) return;
+
         try {
             const rule = extractRule(line);
             cal.addReplacementRule(rule.head, rule.tail);
-        }catch(e) {
+        } catch (e) {
             calibrationMolecule = line.trim();
         }
-
     });
 
     await events.once(rl, "close");
 
-    let nbTotRepl = 0;
-    for(const _ of cal.computeAllReplacement(calibrationMolecule)) {
-        nbTotRepl++;
-    }
-
-    console.log(`Solution 1 : ${nbTotRepl}`);
-    console.log(`Solution 2 : ${fabricate(calibrationMolecule, cal)}`);
-
+    console.log(`Solution 1 : ${getNbAllReplacements(calibrationMolecule)}`);
+    console.log(`Solution 2 : ${monteCarloSearch(calibrationMolecule, cal)}`);
 })();
